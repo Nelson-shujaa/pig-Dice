fd9289e8e4e68863bf3d6af742185a19ee50d395
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-03-01 18:05:51 +0300

    include alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+  function dice (Name,Player) {
+      Name = name;
+      Player = player;
 
+  }
 // var  player1 = [0];
 // var player2 = [0];
 // var game = player1
  function Try() {
-   var player1 = Math.floor(Math.random()*5)+1;
+   var play = [];
+   var player1 = Math.floor(Math.random()*6)+1;
    document.getElementById('p1').innerHTML = player1;
+    play.push(player1);
+    if (play==1) {
+      alert("OOps stop");
+
+
+    }
  }
 
 // $("#p1").submit(function(){
@@ -13,6 +24,11 @@
 //   document.getElementById('p1').innerHTML = player1
 // });
 function Try2(){
-  var player2 = Math.floor(Math.random()*5)+1;
+  var play = [];
+  var player2 = Math.floor(Math.random()*6)+1;
   document.getElementById('p2').innerHTML = player2;
+  play.push(player2);
+  if (play==1);{
+  alert("OOps stop");
+}
 }
