77c1f8ccb5a850cdd275936d947fd46b92291127
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-03-01 17:21:20 +0300

    make button functional

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+// var  player1 = [0];
+// var player2 = [0];
+// var game = player1
+ function Try() {
+   var player1 = Math.floor(Math.random()*5)+1;
+   document.getElementById('p1').innerHTML = player1;
+ }
+
+// $("#p1").submit(function(){
+//   event.preventDefault();
+//   var player1 = Math.floor(Math.random()*6);
+//   document.getElementById('p1').innerHTML = player1
+// });
+function Try2(){
+  var player2 = Math.floor(Math.random()*5)+1;
+  document.getElementById('p2').innerHTML = player2;
+}
