fd15e4eb1eca99358cc88ed43913ecd88ce695db
Nelson Gacheru <nelsonzachariah@gmail.com>
2019-03-05 12:51:41 +0300

    new button

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-  function dice (Name,Player) {
-      Name = name;
-      Player = player;
 
-  }
+
+  //Business logic
   var total = 0;
   var totalscore = 0;
   var total1 = 0;
   var totalscore1 = 0;
+
+  //User interface logic
+  function start() {
+    location.reload();
+    $(".intro").toggle(500);
+
+  }
+  function show() {
+    $(".intro").toggle(500);
+    $(".row").toggle(500);
+
+  }
  function Try() {
    var player1 = 0;
    var player1 = Math.floor(Math.random()*6)+1;
    document.getElementById('p1').innerHTML = player1;
-    //play.push(player1);
-    //total.push(player1);
-    //alert(total);
+
     $("#p2").hide();
     $("#h2").hide();
     if (player1==1) {
@@ -38,17 +46,22 @@
 function hold() {
 totalscore+=total
 $(".scorei").text(totalscore);
-$("#hold").toggle(1500);
-$("#hold").toggle(1500);
+$("#hold").toggle(1000);
+$("#hold").toggle(1000);
 $("#p2").show();
 $("#h2").show();
 $("#p1").hide();
 $("#h1").hide();
-// if (totalscore==100){
-//   $(".win1").toggle();
-//   $(".win1").toggle();
+
 total=0;
 $(".score").text(total);
+if (totalscore>=100) {
+  $(".row").hide();
+  $(".winner").toggle(2000);
+  $(".winner").toggle(3000);
+  $("#rs").toggle(5000);
+
+}
 }
 
 
@@ -63,8 +76,8 @@ function Try2(){
   $("#h1").show();
   $("#p2").hide();
   $("#h2").hide();
-  $("#next").toggle(1500);
-  $("#next").toggle(1500);
+  $("#next").toggle(1000);
+  $("#next").toggle(1000);
   player2=0;
 
 }
@@ -77,18 +90,21 @@ else {
 
 function hold2(){
   totalscore1+=total1;
-  //(".score1").text(total);
   $(".score2").text(totalscore1);
   $("#p1").show()
   $("#h1").show();
   $("#p2").hide();
   $("#h2").hide();
-  $("#hold").toggle(1500);
-  $("#hold").toggle(1500);
-  // if (totalscore==100){
-  //   $("winner.win2").toggle();
-  //   $(".win2").toggle();
-  // }
+  $("#hold").toggle(1000);
+  $("#hold").toggle(1000);
+
     total1=0;
-  $(".score1").text(totalscore1);
+  $(".score1").text(total1);
+  if (totalscore1>=100) {
+    $(".row").hide();
+    $(".win2").toggle(2000);
+    $(".win2").toggle(3000);
+    $("#rs").toggle(5000);
+  }
+
 }
